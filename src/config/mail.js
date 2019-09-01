787b2380804bca7ac4adeb1638e23b54d8140fb1
Adan Felipe Medeiros <adan.grg@gmail.com>
2019-09-01 20:54:46 -0300

    Adiciondo recurso de cancelamento de agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '5a5b2d0cb7e1e0',
+    pass: '2f93ccfca0ecc5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

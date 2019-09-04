7ae5cffb01536555c08b37ab2459b55a6ac771af
Adan Felipe Medeiros <adan.grg@gmail.com>
2019-09-03 21:07:04 -0300

    Adicionado recurso de Jobs para disparo de email

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 create container postgres
 
 docker run --name database -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+
+docker start mongobarber
+docker start postgres
+docker start redisbarber
--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,8 +6,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -134,6 +134,7 @@ class AppointmentController {
       ],
     });
 
+    console.log(appointment.user_id, req.userId);
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment.",
@@ -152,15 +153,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: `Agendamento cancelado`,
-      template: 'cancellation',
-      context: {
-        provider: appointment.providr.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', as' H:mm'h'", pt),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
